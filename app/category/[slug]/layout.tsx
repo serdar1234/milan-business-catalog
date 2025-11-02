cc2867f52b6ee6a557bac38392f61d8109cda7a1
Serdar Geldyev <serdarabad@gmail.com>
2025-11-03 00:17:58 +0300

    fix(categoryLayout): async slug bug fixed

--- app/category/[slug]/layout.tsx
+++ app/category/[slug]/layout.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import { CategoryHeader } from '@/layers/01_widgets/CategoryHeader/CategoryHeader';
+import { useParams } from 'next/navigation';
 
 interface CategoryLayoutProps {
   children: React.ReactNode;
-  params: { slug: string };
 }
 
 const MOCK_MAIN_CATEGORY = {
@@ -10,41 +12,28 @@ const MOCK_MAIN_CATEGORY = {
   name: 'Food & Drink',
 };
 
-export default async function CategoryLayout({
-  children,
-  params,
-}: CategoryLayoutProps) {
-  // Преобразование slug в читаемое имя (это заглушка, в реальности будет запрос к API)
-  const param = await params;
-  const subcategorySlug = param.slug;
-  const subcategoryName = subcategorySlug
-    .replace(/-/g, ' ')
-    .split(' ')
-    .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
-    .join(' ');
+export default function CategoryLayout({ children }: CategoryLayoutProps) {
+  const { slug } = useParams<{ slug: string }>();
+
+  const subcategoryName =
+    (slug &&
+      slug.replace(/-/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase())) ||
+    '';
 
   const breadcrumbs = [
     { label: 'Home', href: '/' },
-    // Главная Категория (Food & Drink)
     {
       label: MOCK_MAIN_CATEGORY.name,
       href: `/category/${MOCK_MAIN_CATEGORY.slug}`,
     },
-    // Подкатегория (Restaurants)
-    { label: subcategoryName, href: `/category/${subcategorySlug}` },
+    { label: subcategoryName, href: `/category/${slug}` },
   ];
-
-  // const handleFilterChange = (filter: string) => {
-  //     console.log(`Filter applied in CategoryLayout: ${filter}`);
-  //     // Здесь будет логика применения фильтра к странице.
-  // };
 
   return (
     <>
       <CategoryHeader
         categoryName={subcategoryName}
         breadcrumbs={breadcrumbs}
-        // onFilterChange={handleFilterChange}
       />
       {children}
     </>
