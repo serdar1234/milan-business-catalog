be2db2b6804424dda29cdd450cb4a5bdc93da894
Serdar Geldyev <serdarabad@gmail.com>
2025-11-04 19:22:56 +0300

    feat(ui): category grid structure

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,3 +1,48 @@
+import { Box, Container } from '@mui/material';
+import style from './CategoryPage.module.css';
+import { MOCK_BUSINESS_DETAILS } from '@/layers/03_entities/business/api/mockData';
+
+export function generateMetadata() {
+  return {
+    title: MOCK_BUSINESS_DETAILS.name,
+    description: MOCK_BUSINESS_DETAILS.description,
+  };
+}
+
 export default function CategoryPage() {
-  return <h2 style={{ margin: '1rem' }}>CategoryPage</h2>;
+  return (
+    <Box component="section" sx={{ py: 3, bgcolor: 'background.default' }}>
+      <Container maxWidth="lg">
+        <div className={style['grid-container']}>
+          <div className={style['grid-item__categories']}>
+            <Box>
+              <h2>Category Select</h2>
+            </Box>
+          </div>
+          <div className={style['grid-item__filters']}>
+            <Box
+              sx={{
+                width: '100%',
+                height: { md: '100%' },
+              }}
+            >
+              <h2>Filters</h2>
+            </Box>
+          </div>
+          <div className={style['grid-item__main']}>
+            <Box>
+              <h2>Category Name</h2>
+              <h2>Category Page</h2>
+              <h2>Category Name</h2>
+              <h2>Category Page</h2>
+              <h2>Category Name</h2>
+              <h2>Category Page</h2>
+              <h2>Category Name</h2>
+              <h2>Category Page</h2>
+            </Box>
+          </div>
+        </div>
+      </Container>
+    </Box>
+  );
 }
--- layers/01_widgets/CategoryHeader/CategoryHeader.tsx
+++ layers/01_widgets/CategoryHeader/CategoryHeader.tsx
@@ -7,11 +7,13 @@ import { FilterButtons } from '@/layers/04_shared/ui/FilterButtons';
 
 interface CategoryHeaderProps {
   categoryName: string;
+  placeCount?: number;
   breadcrumbs?: BreadcrumbItem[];
 }
 
 export const CategoryHeader: React.FC<CategoryHeaderProps> = ({
   categoryName,
+  placeCount = 10,
   breadcrumbs = [],
 }) => {
   return (
@@ -22,13 +24,14 @@ export const CategoryHeader: React.FC<CategoryHeaderProps> = ({
       }}
     >
       <Box maxWidth="lg" sx={{ mx: 'auto' }}>
+        {/* Breadcrumbs */}
         {breadcrumbs.length > 0 && (
           <Box>
             <AppBreadcrumbs items={breadcrumbs} />
           </Box>
         )}
 
-        {/* Header and filter buttons */}
+        {/* Name and filter buttons */}
         <Stack
           direction={{ xs: 'column', md: 'row' }}
           justifyContent="space-between"
@@ -36,11 +39,16 @@ export const CategoryHeader: React.FC<CategoryHeaderProps> = ({
           spacing={{ xs: 2, md: 0 }}
           sx={{ p: { xs: 2, md: '1.5rem' } }}
         >
-          {/* Name */}
+          {/* Category Name */}
           <Typography variant="h4" component="h1" fontWeight="bold">
             {categoryName}
+            {placeCount && (
+              <Typography variant="body1" component="div">
+                {placeCount + ' places'}
+              </Typography>
+            )}
           </Typography>
-
+          {/* Filter buttons */}
           <FilterButtons />
         </Stack>
       </Box>
