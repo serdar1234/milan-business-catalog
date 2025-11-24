3ed31d73400a97a8e34d3c6a6d84dc7a886da9a5
Serdar Geldyev <serdarabad@gmail.com>
2025-11-24 21:59:50 +0300

    feat(lang): Slovak added!

--- layers/04_shared/configs/settings.ts
+++ layers/04_shared/configs/settings.ts
@@ -1,10 +1,11 @@
-export type LanguageCode = 'en' | 'es' | 'it';
+export type LanguageCode = 'en' | 'es' | 'it' | 'sk';
 export type CurrencyCode = 'EUR' | 'USD' | 'GBP';
 
 export const LANGUAGES: { code: LanguageCode; name: string }[] = [
   { code: 'en', name: 'English' },
   { code: 'es', name: 'Español' },
   { code: 'it', name: 'Italiano' },
+  { code: 'sk', name: 'Slovenský' },
 ];
 
 export const CURRENCIES: { code: CurrencyCode; name: string }[] = [
